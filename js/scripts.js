154bb459fe6c5556e5fe39931e05532caefc57be
brandontyruspetty <tyrus1974@gmail.com>
2022-09-09 11:11:11 -0700

    Created variable pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'peanut-butter apples';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Dewgong', type: ['ice', 'water'], height: 1.7},
+    {name: 'Bastiodon', type: ['steel', 'rock'], height: 1.3},
+    {name: 'Woobat', type: ['psychic', 'flying'], height: .4},
+];
