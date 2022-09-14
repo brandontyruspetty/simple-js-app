8a049a68da532c643771807b460500c2e3fdf6d8
brandontyruspetty <tyrus1974@gmail.com>
2022-09-14 11:01:54 -0700

    Created for loop and conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,21 @@ let pokemonList = [
     {name: 'Bastiodon', type: ['steel', 'rock'], height: 1.3},
     {name: 'Woobat', type: ['psychic', 'flying'], height: .4},
 ];
+
+
+
+// create for loop for pokemon iteration
+for(let i = 0; i < pokemonList.length; i++) {
+    //create name/height variable
+let pokemonNameHeight = pokemonList[i].name + ' (height: ' +  pokemonList[i].height  + ')';  
+// print conditionals
+if(pokemonList[i].height > 1.5){
+    document.write('<p>' + pokemonNameHeight  + ' -  WOW! That\'s big! </p>');
+} else if(pokemonList[i].height < .5){
+    document.write('<p>' + pokemonNameHeight  + ' - He\'s so cute! <p>'); 
+}else{
+    document.write('<p>' +  pokemonNameHeight + '</p>');
+}
+}
+
+
