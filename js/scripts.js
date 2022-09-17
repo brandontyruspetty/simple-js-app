76b2b7f648b994765175a06321c8a49f94abecac
brandontyruspetty <tyrus1974@gmail.com>
2022-09-17 10:47:23 -0700

    Wrapped pokemonList array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
+
+(function(){
 let pokemonList = [
     {name: 'Dewgong', type: ['ice', 'water'], height: 1.7},
     {name: 'Bastiodon', type: ['steel', 'rock'], height: 1.3},
     {name: 'Woobat', type: ['psychic', 'flying'], height: .4},
 ];
+})()
 
 
 
