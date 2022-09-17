55c572ddb3041f2e9d29257e58deb3ed21d01b25
brandontyruspetty <tyrus1974@gmail.com>
2022-09-17 12:45:39 -0700

    Create pokemonRepository IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,37 @@ let pokemonList = [
 ];
 })()
 
+//create pokemonRepository variable
+let pokemonRepository = (function() {
+    let pokemonList = [];
+//create a return for key-value pairs
+        function add(pokemon) {
+            pokemonList.push(pokemon);
+        }
+        function getAll() {
+            return pokemonList;
+        }
+        return {
+            add: add,
+            getAll: getAll
+        };
+})()
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: 'Pikachu'});
+console.log(pokemonRepository.getAll());
 
 
 
-pokemonList.forEach(function(pokemon) {
+// pokemonList.forEach(function(pokemon) {
     
-let pokemonNameHeight = pokemon.name + ' (height: ' +  pokemon.height  + ')';  
-
-if(pokemon.height > 1.5){
-    document.write('<p>' + pokemonNameHeight  + ' -  WOW! That\'s big! </p>');
-} else if(pokemon.height < .5){
-    document.write('<p>' + pokemonNameHeight  + ' - He\'s so cute! <p>'); 
-}else{
-    document.write('<p>' +  pokemonNameHeight + '</p>');
-}
-});
-
+// let pokemonNameHeight = pokemon.name + ' (height: ' +  pokemon.height  + ')';  
+// if(pokemon.height > 1.5){
+//     document.write('<p>' + pokemonNameHeight  + ' -  WOW! That\'s big! </p>');
+// } else if(pokemon.height < .5){
+//     document.write('<p>' + pokemonNameHeight  + ' - He\'s so cute! <p>'); 
+// }else{
+//     document.write('<p>' +  pokemonNameHeight + '</p>');
+// }
+// });
 
 
