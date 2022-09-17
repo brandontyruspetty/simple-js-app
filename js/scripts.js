d1dcdb3d6e2ece87a1150b99e3ad6ac4b82cdd32
brandontyruspetty <tyrus1974@gmail.com>
2022-09-17 14:01:17 -0700

    Redo of assigning IIFE to pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,38 @@
-
-(function(){
-let pokemonList = [
+//create pokemonRepository variable
+let pokemonRepository = (function(){
+let pokemonList= [
     {name: 'Dewgong', type: ['ice', 'water'], height: 1.7},
     {name: 'Bastiodon', type: ['steel', 'rock'], height: 1.3},
     {name: 'Woobat', type: ['psychic', 'flying'], height: .4},
 ];
+//create a return for key-value pairs
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
+function getAll() {
+    return pokemonList;
+}
+return {
+    add: add,
+    getAll: getAll
+};
 })()
 
-//create pokemonRepository variable
-let pokemonRepository = (function() {
-    let pokemonList = [];
-//create a return for key-value pairs
-        function add(pokemon) {
-            pokemonList.push(pokemon);
-        }
-        function getAll() {
-            return pokemonList;
-        }
-        return {
-            add: add,
-            getAll: getAll
-        };
-})()
-console.log(pokemonRepository.getAll());
 pokemonRepository.add({name: 'Pikachu'});
 console.log(pokemonRepository.getAll());
 
 
-
-// pokemonList.forEach(function(pokemon) {
+//cannot figure out how to link to the IIFE above
+pokemonList.forEach(function(pokemon) {
     
-// let pokemonNameHeight = pokemon.name + ' (height: ' +  pokemon.height  + ')';  
-// if(pokemon.height > 1.5){
-//     document.write('<p>' + pokemonNameHeight  + ' -  WOW! That\'s big! </p>');
-// } else if(pokemon.height < .5){
-//     document.write('<p>' + pokemonNameHeight  + ' - He\'s so cute! <p>'); 
-// }else{
-//     document.write('<p>' +  pokemonNameHeight + '</p>');
-// }
-// });
+let pokemonNameHeight = pokemon.name + ' (height: ' +  pokemon.height  + ')';  
+if(pokemon.height > 1.5){
+    document.write('<p>' + pokemonNameHeight  + ' -  WOW! That\'s big! </p>');
+} else if(pokemon.height < .5){
+    document.write('<p>' + pokemonNameHeight  + ' - He\'s so cute! <p>'); 
+}else{
+    document.write('<p>' +  pokemonNameHeight + '</p>');
+}
+});
 
 
