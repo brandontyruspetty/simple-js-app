000f1084c91617329a925c91104510dc4194d788
brandontyruspetty <tyrus1974@gmail.com>
2022-09-23 15:13:37 -0700

    created  buttons for individual pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -9,29 +9,42 @@ function add(pokemon) {
 function getAll() {
     return pokemonList;
 }
+
+function showDetails(pokemon){
+    console.log(pokemon)
+};
+
+function addListItem(pokemon){
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    //create button for individual pokemons
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokebutton');
+    //attach button to listItem
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    //added listening event for button to write pokemon name in console when clicked
+    button.addEventListener('click', function(event){  
+        showDetails(pokemon); 
+    });
+};
+
 return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem,
 };
 })()
-
-// console.log(pokemonRepository)
 
 pokemonRepository.add({name: 'Dewgong', type: ['ice', 'water'], height: 1.7});
 pokemonRepository.add({name: 'Bastiodon', type: ['steel', 'rock'], height: 1.3});
 pokemonRepository.add({name: 'Woobat', type: ['psychic', 'flying'], height: .4});
 pokemonRepository.add({name: 'Pikachu'});
 
+console.log(pokemonRepository.getAll());
 
 
 pokemonRepository.getAll().forEach(function(pokemon) {
-    
-let pokemonNameHeight = pokemon.name + ' (height: ' +  pokemon.height  + ')';  
-if(pokemon.height > 1.5){
-    document.write('<p>' + pokemonNameHeight  + ' -  WOW! That\'s big! </p>');
-} else if(pokemon.height < .5){
-    document.write('<p>' + pokemonNameHeight  + ' - He\'s so cute! <p>'); 
-}else{
-    document.write('<p>' +  pokemonNameHeight + '</p>');
-}
+    pokemonRepository.addListItem(pokemon)
 });
